d418cf19b7e5713eced59da8738d4db03a1385a4
Kaer <kaerlaende@mail.ru>
2023-08-23 10:16:05 +0300

    Завершения основных частей приложения.

--- src/main/java/com/github/KaerLaende/NewsApp/service/CategoryService.java
+++ src/main/java/com/github/KaerLaende/NewsApp/service/CategoryService.java
@@ -3,16 +3,38 @@ import com.github.KaerLaende.NewsApp.DTO.CategoryDto;
 
 import java.util.List;
 
+/**
+ * Данный класс реализуется сервис для работы с категориями новостей.
+ * Содержит необходимые зависимости, такие как  CategoryRepository  и  CategoryMapper,
+ * которые используются для взаимодействия с базой данных и маппинга объектов категорий.
+ */
 public interface CategoryService {
 
-
+    /**
+     * возвращает список всех категорий новостей в виде объектов  CategoryDto
+     */
     List<CategoryDto> findAllCategory();
 
+    /**
+     * добавляет новую категорию с указанным названием и возвращает объект  CategoryDto  новой категории.
+     */
     CategoryDto addCategory(String categoryName);
 
+    /**
+     * редактирует категорию с указанным идентификатором, меняя ее название на указанное, и возвращает объект CategoryDto  отредактированной категории.
+     * Если категория с указанным идентификатором не найдена, выбрасывается исключение CategoryNotFoundException.
+     */
     CategoryDto editCategory(long id, String categoryName);
 
+    /**
+     *  удаляет категорию с указанным идентификатором и возвращает объект  CategoryDto  удаленной категории.
+     *  Если категория с указанным идентификатором не найдена, выбрасывается исключение  CategoryNotFoundException.
+     */
     CategoryDto deleteCategory(long id);
 
+    /**
+     * проверяет, существует ли категория с указанным объектом  CategoryDto.
+     * Возвращает true, если категория существует, иначе возвращает  false.
+     */
     boolean checkCategoryExists(CategoryDto categoryDto);
 }
--- src/main/java/com/github/KaerLaende/NewsApp/service/NewsService.java
+++ src/main/java/com/github/KaerLaende/NewsApp/service/NewsService.java
@@ -5,12 +5,33 @@ import com.github.KaerLaende.NewsApp.DTO.EditNewsDto;
 import com.github.KaerLaende.NewsApp.DTO.NewsDto;
 import com.github.KaerLaende.NewsApp.DTO.ResponseWrapperNewsDto;
 
+/**
+ * Данный класс реализуется сервис для работы с новостями. Содержит необходимые зависимости, такие как NewsRepository
+ * и NewsMapper, которые используются для взаимодействия с базой данных и маппинга объектов новостей.
+ */
+
 public interface NewsService {
+    /**
+     * добавляет новость на основе переданного объекта CreateNewsDto и возвращает объект NewsDto добавленной новости.
+     */
     NewsDto addNews (CreateNewsDto createNewsDto);
 
+    /**
+     * возвращает отфильтрованный список новостей на основе переданных параметров категории, названия и содержания
+     * новостей. Результат возвращается в виде объекта ResponseWrapperNewsDto.
+     */
     ResponseWrapperNewsDto getFilteredNews(String category, String name, String content);
 
+    /**
+     * редактирует новость с указанным идентификатором, изменяя ее название и/или содержание. Возвращает объект
+     * EditNewsDto отредактированной новости. Если новость с указанным идентификатором не найдена,
+     * выбрасывается исключение NewsNotFoundException.
+     */
     EditNewsDto editNews(long id, String title, String content);
 
+    /**
+     * удаляет новость с указанным идентификатором и возвращает объект NewsDto удаленной новости.
+     * Если новость с указанным идентификатором не найдена, выбрасывается исключение NewsNotFoundException .
+     */
     NewsDto deleteNews(long id);
 }
